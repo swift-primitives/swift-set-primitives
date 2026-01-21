dc45e5f71f9604d75bda6c6ef3a4ffdeadb5cb54
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-21 10:23:44 +0100

    Add Collection Primitives dependency
    Sets are collections and should depend on Collection Primitives for
    collection view types and transformations.

--- Package.swift
+++ Package.swift
@@ -18,13 +18,21 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "../swift-standard-library-extensions")
+        .package(path: "../swift-standard-library-extensions"),
+        .package(path: "../swift-bit-primitives"),
+        .package(path: "../swift-index-primitives"),
+        .package(path: "../swift-input-primitives"),
+        .package(path: "../swift-collection-primitives"),
     ],
     targets: [
         .target(
             name: "Set Primitives",
             dependencies: [
-                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions")
+                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
+                .product(name: "Bit Primitives", package: "swift-bit-primitives"),
+                .product(name: "Index Primitives", package: "swift-index-primitives"),
+                .product(name: "Input Primitives", package: "swift-input-primitives"),
+                .product(name: "Collection Primitives", package: "swift-collection-primitives"),
             ]
         ),
         .testTarget(
